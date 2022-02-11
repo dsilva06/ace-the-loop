4ebd4f20e8d39cbf04f2e7cd39093384173dff14
Diego Silva <Diegosilva06092001@gmail.com>
2022-02-11 13:28:58 -0400

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("ace-the-loop JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/ace-the-loop";
+
+module.exports = MONGO_URI;
